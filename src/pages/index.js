b5e6ebbb1575a166845b4ee39df3f8c6856b358b
Jonathan Dorety <jonathan.dorety@gmail.com>
2021-01-28 23:08:23 -0500

    Attempt blog post list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,36 @@
 import * as React from "react";
-import Header from "../components/Header/Header";
+import Layout from "../components/Layout/Layout";
+import { graphql, Link, useStaticQuery } from "gatsby";
 // styles
 
 // markup
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      allMdx {
+        nodes {
+          id
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  console.log(data.allMdx);
+
   return (
     <main>
-      <Header />
+      <Layout>
+        {data.allMdx.nodes &&
+          data.allMdx.nodes.map((post) => (
+            <Link to={post.frontmatter.path}>
+              <h3>{post.frontmatter.title}</h3>
+            </Link>
+          ))}
+      </Layout>
     </main>
   );
 };
